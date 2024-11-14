37ee4886948be13c6d138b40210ddf8d3c188d62
kamranlatifch <kamranlatif98@gmail.com>
2024-11-14 17:16:22 +0500

    add hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className='px-[19rem] py-[6.25rem]'>
+      <div className='flex justify-center items-center flex-col gap-[2.5rem]'>
+        <h1 className='font-montserrat text-[3.5rem] font-bold text-center'>
+          Get It Together And Manage Projects the Right Way.
+        </h1>
+        <p className='font-raleway text-lg font-normal text-center w-[580px]'>
+          udix is the world’s first smart workspace. We bring all your team’s
+          content together while letting you use the tools you love.
+        </p>
+        <div className='flex gap-[24px]'>
+          <input
+            type='email'
+            placeholder='Email'
+            className='border border-bgGray rounded  w-[350px] h-[50px] px-[40px] font-raleway text-[16px] font-normal'
+          />
+          <button className='bg-primary rounded text-textBase font-raleway text-base font-medium px-[38px] '>
+            Subscribe
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
